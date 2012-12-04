e553a0d642d9426e08e214217a0115fa26b7a11a
Monzen <monzen@srl-tcs.co.jp>
2012-12-04 09:10:02 +0900

    first master modification (v0.0.0)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trac::Application.config.secret_token = 'ce57ed99639b211d929bf47fb3cdf8eeebdd93da275608bd1fa74eb508804ed4af5d7be024dafff7e1b52216179bf1d3e4e74f40512c6557b42dd03b0f73939b'
